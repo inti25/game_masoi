e02c983ccc834c655602b4b2408b290df8a08d3f
inti25 <hungnq9.tcb@gmail.com>
2021-08-29 15:55:27 +0700

    uodate env

--- public/services/MaSoiServices.js
+++ public/services/MaSoiServices.js
@@ -3,7 +3,7 @@ import {setLoading} from "./../core/fabrics.js";
 export default class MaSoiServices {
 
   constructor() {
-    this.baseUrl = "http://localhost:3000";
+    this.baseUrl = process.env.ENV == 'PROD' ? "https://game-masoi.herokuapp.com" : "http://localhost:3000";
   }
 
   async checkUserByPhone(phone_number) {
